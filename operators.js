f907be3584c995285b1d6700b4c74a5b31bedc67
perarasua25csc-pixel <perarasua25csc@vetias.ac.in>
2026-06-12 11:01:05 +0530

    operators.js

---
+++ operators.js
@@ -0,0 +1,19 @@
+
+var a=90
+var b=60
+console.log(a-b) 
+
+var a=80
+var b=50
+console.log(a*b) //multiple
+ 
+var a=60
+var b=20
+console.log(a/b) //division
+ 
+var a=10
+var b=60
+console.log(a%b) //modulus
+  
+var a=30
+console.log(a**2) //exponential
