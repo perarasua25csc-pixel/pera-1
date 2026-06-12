6b8ab3dd45cb2c13b729dbc8595ea3a710e40cf7
perarasua25csc-pixel <perarasua25csc@vetias.ac.in>
2026-06-12 12:11:30 +0530

    operators.js

--- operators.js
+++ operators.js
@@ -1,5 +1,5 @@
 
-/*console.log(Arithemetic operator)
+console.log("Arithemetic operator")
 var a=90
 var b=60
 console.log(a-b)
@@ -27,7 +27,7 @@ console.log(a-=20)
 console.log(a*=20)
 console.log(a/=20)
 
-//comparision operator
+//compare operator
 console.log("compare operator")
 var a=90
 var b=60
@@ -37,3 +37,17 @@ console.log(a<b)
 console.log(a>b)
 console.log(a<=b)
 console.log(a>=b)
+
+
+//logical operator
+console.log("Logical operator")
+var a=60
+var b=20
+console.log(a<3 && b<9)
+console.log(a<7 || b>6)
+console.log(!(a<3))
+console.log(a>3 && b>9)
+console.log(a>7 || b<6)
+console.log(!(a<3))
+
+
