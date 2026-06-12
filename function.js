eb10c38c0e0a2b94bdda45ae6da20d515dc147c8
perarasua25csc-pixel <perarasua25csc@vetias.ac.in>
2026-06-12 15:52:35 +0530

    function.js

---
+++ function.js
@@ -0,0 +1,9 @@
+//Functions
+var factor = "THALA";
+var fplayer = "DHONI";
+var fmovie = "VISHWASAM";
+
+function favorite() {
+    return "My favorite actor is " + factor + ", my favorite player is " + fplayer + ", and my favorite movie is " + fmovie + ".";
+}
+console.log(favorite());
