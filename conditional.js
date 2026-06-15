15c2a4792f3929304f1fc985cfb2c131910f0bcb
perarasua25csc-pixel <perarasua25csc@vetias.ac.in>
2026-06-15 10:37:23 +0530

    conditional.js

---
+++ conditional.js
@@ -0,0 +1,18 @@
+var signal ="White"
+switch(signal){
+    case "green":
+    console.log("Go!")
+    break;
+    
+    case "Yellow":
+        console.log("Wait")
+        break;
+         
+    case "red":
+        console.log("Stop")
+        break;
+
+    default:
+        console.log("Invalid color light")    
+
+}
